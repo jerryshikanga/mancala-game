c34e77fa0ed056fde2f8a9e2d998000022acf9bd
Jerry Shikanga <jerryshikanga@gmail.com>
2023-05-16 11:12:29 +0300

    Add extra logging points for debug purposes

--- src/main/java/com/shikanga/mancala/businesslogic/Game.java
+++ src/main/java/com/shikanga/mancala/businesslogic/Game.java
@@ -3,6 +3,8 @@ package com.shikanga.mancala.businesslogic;
 import com.shikanga.mancala.exceptions.EmptyPitException;
 import com.shikanga.mancala.exceptions.InvalidPitException;
 import com.shikanga.mancala.exceptions.InvalidPlayerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 
@@ -10,6 +12,8 @@ public class Game {
     private final int NUM_PLAYERS = 2;
     private final int NUM_PITS = 6;
     private final int BIG_PIT_INDEX = NUM_PITS;
+
+    private final Logger logger = LoggerFactory.getLogger(Game.class);
 
     private int[][] board;
     private int currentPlayer;
@@ -53,12 +57,15 @@ public class Game {
 
     public void validateMove(int player, int pitIndex){
         if (player < 0 || player >= NUM_PLAYERS){
+            logger.error("The player ID " + player + " is invalid.");
             throw new InvalidPlayerException("The player ID " + player + " is invalid.");
         }
         if (pitIndex < 0 || pitIndex > BIG_PIT_INDEX){
+            logger.error("The pit index " + pitIndex + " is invalid.");
             throw new InvalidPitException("The pit index " + pitIndex + " is invalid.");
         }
         if (this.board[player][pitIndex] <= 0){
+            logger.error("The pit is empty!");
             throw new EmptyPitException("The pit is empty!");
         }
     }
@@ -75,10 +82,12 @@ public class Game {
         int opponentPitStones = board[opponent][opponentCorrespondingPitIndex];
         board[opponent][opponentCorrespondingPitIndex] = 0;
         board[player][BIG_PIT_INDEX] += opponentPitStones;
+        logger.info("Captured "+ opponentPitStones + " stones from opponent pit "+opponentCorrespondingPitIndex);
     }
 
     public int makeMove(int pitIndex) {
         this.validateMove(currentPlayer, pitIndex);
+        logger.debug("Move "+ pitIndex +" by player " + currentPlayer+ " validated");
         int currentBoard = currentPlayer;
         int currentStoneCount = board[currentBoard][pitIndex];
         board[currentBoard][pitIndex] = 0;
@@ -89,6 +98,7 @@ public class Game {
         while (currentStoneCount > 0){
             // Handle a case where we should not sow in the opponents BigPit
             if (currentBoard != currentPlayer && currentPitIndex == BIG_PIT_INDEX){
+                logger.debug("Next pit is opponents big pit. Skipping.");
                 currentBoard = this.switchBoardOrPlayer(currentBoard);
                 currentPitIndex = 0;
                 continue;
@@ -102,9 +112,11 @@ public class Game {
             // run a few extra checks if this is the case
             // these checks have to be run before switching boards or incrementing the currentPitIndex otherwise we end up with wrong data
             if (currentStoneCount == 0 && currentBoard == currentPlayer){
+                logger.debug("Last stone landed in current player's side of the board. Running extra checks.");
                 // Try to determine the next player
                 // the next player is always the opponent unless the current player last stone lands in his own big pit
                 if (currentPitIndex == BIG_PIT_INDEX){
+                    logger.debug("Last stone landed in current player's big pit. He/She has another chance to play.");
                     nextPlayer = currentPlayer;
                 }
 
@@ -113,6 +125,7 @@ public class Game {
                 // If this is the case then we can capture the stones in the opponents pit opposite the current pit
                 int numberOfStonesInCurrentPitAfterIncrementing = board[currentBoard][currentPitIndex];
                 if (currentPitIndex != BIG_PIT_INDEX && numberOfStonesInCurrentPitAfterIncrementing == 1){
+                    logger.info("Last stone landed in a pit belonging to the current player that is empty. Will begin capture of opponent's stones.");
                     this.captureFromOpponentOppositePit(currentPlayer, currentPitIndex);
                 }
             }
@@ -122,6 +135,7 @@ public class Game {
 
             // check if we are at the end of the current players pits and move to the opponents pits
             if (currentPitIndex > NUM_PITS){
+                logger.debug("Switching to opponent's side of the board");
                 currentBoard = this.switchBoardOrPlayer(currentBoard);
                 currentPitIndex = 0;
             }
@@ -138,7 +152,7 @@ public class Game {
     public Boolean isGameOver(){
         boolean playerOneHasStones = this.countPlayerStones(0) > 0;
         boolean playerTwoHasStones = this.countPlayerStones(1) > 0;
-
+        logger.debug("playerOneHasStones "+playerOneHasStones+ " playerTwoHasStones "+playerTwoHasStones);
         return !playerOneHasStones || !playerTwoHasStones;
     }
 }
