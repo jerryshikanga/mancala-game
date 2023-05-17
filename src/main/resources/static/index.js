1f3cbc5e23ed83bcd4817ff55f60e7a98227a34e
Jerry Shikanga <jerryshikanga@gmail.com>
2023-05-17 13:05:15 +0300

    Add modal for errors, use bootstrap for game styling

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -5,23 +5,7 @@ $(document).ready(function() {
         var board = response.board;
         var currentPlayer = response.currentPlayer;
 
-        // Add player one pits
-        var topRow = $("<tr></tr>");
-        for (var i = 0; i < 6; i++) {
-            var cell = $("<td></td>").text(board[0][i]);
-            topRow.append(cell);
-        }
-
-        // Add player two pits
-        var bottomRow = $("<tr></tr>");
-        for (var i = 5; i >= 0; i--) {
-            var cell = $("<td></td>").text(board[1][i]);
-            bottomRow.append(cell);
-        }
-
-        $("#gameTable tbody").append(topRow, bottomRow);
-        $("#playerOneMancala").text(board[0][6]);
-        $("#playerTwoMancala").text(board[1][6]);
+        renderBoard(board);
     }).fail(function(jqXHR, textStatus, errorThrown) {
         var errorResponse = jqXHR.responseJSON;
 
@@ -29,5 +13,58 @@ $(document).ready(function() {
         console.log("Error:", errorResponse.title);
         console.log("Status:", errorResponse.status);
         console.log("Detail:", errorResponse.detail);
+
+        showErrorMessage(errorResponse.detail);
     });
 });
+
+function renderBoard(board){
+    const BIG_PIT_INDEX = 6;
+    const NUM_PITS = 6;
+    // Add player one pits
+    var topRow = $('<tr></tr>');
+    var playerIndex = 0;
+    for (var i = 0; i < NUM_PITS; i++) {
+        var cell = getCellHtml(i, playerIndex, board[playerIndex][i]);
+        topRow.append(cell);
+    }
+
+    // Add player two pits
+    var bottomRow = $('<tr></tr>');
+    playerIndex = 1;
+    for (var i = NUM_PITS-1; i >= 0; i--) {
+        var cell = getCellHtml(i, playerIndex, board[playerIndex][i]);
+        bottomRow.append(cell);
+    }
+
+    $("#gameTable tbody").append(topRow, bottomRow);
+    $("#playerOneMancala").text(board[0][BIG_PIT_INDEX]);
+    $("#playerTwoMancala").text(board[1][BIG_PIT_INDEX]);
+}
+
+function showErrorMessage(errorMessage){
+    $('#errorModalDetails').text(errorMessage);
+    $('#errorModal').modal('show');
+}
+
+
+function pitClicked(pitIndex, playerIndex, stoneCount){
+    console.log("Clicked pit with index : "+pitIndex+ " playerIndex : "+playerIndex);
+}
+
+function getCellHtml(cellIndex, playerIndex, stoneCount){
+    var content = '<td>'
+    var buttonClass;
+    if (playerIndex == 1){
+        buttonClass = 'btn btn-success';
+    }
+    else{
+        buttonClass = 'btn btn-secondary'
+    }
+    content += '<button type="button" class=" '+ buttonClass +' " class="text-center"'
+    content += 'onclick="pitClicked('+ cellIndex +', '+ playerIndex +', '+ stoneCount +')">';
+    content += stoneCount;
+    content += '</button>'
+    content += '</td>';
+    return content;
+}
