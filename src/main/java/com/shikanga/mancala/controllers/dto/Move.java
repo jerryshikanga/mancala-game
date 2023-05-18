897e29dbe31fa517672dbbefea0fd4ed45f19278
Jerry Shikanga <jerryshikanga@gmail.com>
2023-05-18 13:51:36 +0300

    Controller tests, mock redis cache, Game.equals(), add flow diagram

--- src/main/java/com/shikanga/mancala/controllers/dto/Move.java
+++ src/main/java/com/shikanga/mancala/controllers/dto/Move.java
@@ -1,9 +1,10 @@
 package com.shikanga.mancala.controllers.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter @Setter @AllArgsConstructor
 public class Move {
     private Integer player;
     private Integer pitIndex;
